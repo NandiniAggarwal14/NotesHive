6e9c5329798d19472e0f3502ef4e26cc5bc45f9b
Nandini Aggarwal <152698318+NandiniAggarwal14@users.noreply.github.com>
2025-10-14 15:20:26 +0530

    Update notehive_values.sql

--- notehive_values.sql
+++ notehive_values.sql
@@ -31,8 +31,3 @@ INSERT INTO Notes (title, description, file_path, subject_id, uploaded_by, uploa
 ('Linked Lists', 'Notes on singly & doubly linked lists', 'https://drive.google.com/file/d/ghi789/view', 2, 3, '2025-10-01'),
 ('OS Presentation', 'Firewall Demonstration', 'https://docs.google.com/presentation/d/1osq5apb7_Ki0AFuBntbIPFWIRWlo_UwU/edit?usp=sharing&ouid=103908473392938725216&rtpof=true&sd=true', 3, 4, '2025-10-06');
 
-INSERT INTO Comments (note_id, user_id, comment_text, date_posted) VALUES
-(1, 1, 'Can you explain semantic search again?', '2025-10-06'),
-(2, 1, 'Thanks, this was helpful!', '2025-10-06'),
-(3, 2, 'Good notes on linked lists!', '2025-10-06'),
-(4, 1, 'This helped me understand firewall working', '2025-10-06');
